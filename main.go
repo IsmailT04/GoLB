2c0ac16314cf8efab3bd15283438afffe109b011
IsmailT0 <ismailtemuroglu2819@gmail.com>
2026-01-20 20:52:44 +0300

    feat: implement reverse proxy

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+const backendURL string = "http://localhost:8081"
 
 func main() {
-	fmt.Println("Hello World")
+	origin, err := url.Parse(backendURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(origin)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		log.Printf("LB Received request: %s %s", r.Method, r.URL.Path)
+
+		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Println("Load Balancer started on :8080 -> Forwarding to :8081")
+
+	// Start the Load Balancer server
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
